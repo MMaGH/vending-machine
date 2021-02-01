c26fbfa1d63d2f11b7cd389e36139430329bf4b6
MMaGH <misk.mardi@gmail.com>
2021-02-01 15:37:22 +0100

    add addCoinIncreasesBalance() test to BalanceTest

--- src/test/java/com/task/vendingmachine/container/BalanceTest.java
+++ src/test/java/com/task/vendingmachine/container/BalanceTest.java
@@ -3,6 +3,21 @@ package com.task.vendingmachine.container;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BalanceTest {
+import com.task.vendingmachine.model.Coin;
 
+
+public class BalanceTest {
+	
+	Balance balance = new Balance();
+	
+	
+	@Test
+	void addCoinIncreasesBalance() {
+		balance.addCoin(Coin.QUARTER);
+		assertEquals(25, balance.getBalance());
+		
+		balance.addCoin(Coin.NICKEL);
+		assertEquals(30, balance.getBalance());
+		
+	}
 }
