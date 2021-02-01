f9aa83f9e85177fb77c8b0a63f1052408651e5c8
MMaGH <misk.mardi@gmail.com>
2021-02-01 20:01:34 +0100

    add getChangeReturnsRemainderChange() test

--- src/test/java/com/task/vendingmachine/container/BalanceTest.java
+++ src/test/java/com/task/vendingmachine/container/BalanceTest.java
@@ -3,21 +3,49 @@ package com.task.vendingmachine.container;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.task.vendingmachine.model.Coin;
 
 
 public class BalanceTest {
 	
-	Balance balance = new Balance();
-	
 	
 	@Test
 	void addCoinIncreasesBalance() {
+		Balance balance = new Balance();
 		balance.addCoin(Coin.QUARTER);
 		assertEquals(25, balance.getBalance());
 		
 		balance.addCoin(Coin.NICKEL);
 		assertEquals(30, balance.getBalance());
-		
 	}
+	
+	@Test
+	void payDecresesBalance() {
+		Balance balance = new Balance();
+		balance.addCoin(Coin.QUARTER);
+		balance.addCoin(Coin.QUARTER);
+		
+		balance.pay(45);
+		
+		assertEquals(5, balance.getBalance());
+	}
+	
+	@Test
+	void getChangeReturnsRemainderChange() {
+		Balance balance = new Balance();
+		
+		balance.addCoin(Coin.QUARTER);
+		balance.addCoin(Coin.QUARTER);
+		balance.addCoin(Coin.NICKEL);
+		
+		balance.pay(35);
+		
+		List<Coin> expectedCoins = Arrays.asList(Coin.NICKEL, Coin.NICKEL);
+		
+		assertEquals(expectedCoins, balance.getChange());
+	}
+	
 }
