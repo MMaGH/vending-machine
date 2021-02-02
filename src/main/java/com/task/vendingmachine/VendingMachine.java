04e8ddea86688ec11d5b80a5f9462c7cd67c543b
MMaGH <misk.mardi@gmail.com>
2021-02-02 16:38:46 +0100

    add constructor and setter to enable DI

--- src/main/java/com/task/vendingmachine/VendingMachine.java
+++ src/main/java/com/task/vendingmachine/VendingMachine.java
@@ -3,10 +3,25 @@ package com.task.vendingmachine;
 import java.util.List;
 import java.util.Map;
 
+import com.task.vendingmachine.container.Balance;
+import com.task.vendingmachine.container.Inventory;
 import com.task.vendingmachine.model.Beverage;
 import com.task.vendingmachine.model.Coin;
 
 public class VendingMachine {
+	private Balance balance;
+	private Inventory inventory;
+	
+	public VendingMachine() {
+		this.balance = new Balance();
+		this.inventory = new Inventory();
+	}
+	
+	public VendingMachine(Balance balance, Inventory inventory) {
+		this.balance = balance;
+		this.inventory = inventory;
+	}
+	
 	
 	
 	public void addFund(Coin coin) {
@@ -26,7 +41,7 @@ public class VendingMachine {
 	}
 	
 	public int getBalance() {
-		return null;
+		return 0;
 	}
 	
 	public List<Coin> getChange() {
@@ -43,6 +58,14 @@ public class VendingMachine {
 	
 	public Map<Beverage, Integer> getConsumption() {
 		return null;
+	}
+	
+	public void setBalance(Balance balance) {
+		this.balance = balance;
+	}
+	
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
 	}
 	
 }
