d0f8ccae557162f65798632da303e5a9be978979
MMaGH <misk.mardi@gmail.com>
2021-02-02 15:19:21 +0100

    add skeleton to VendingMachine class

--- src/main/java/com/task/vendingmachine/VendingMachine.java
+++ src/main/java/com/task/vendingmachine/VendingMachine.java
@@ -1,5 +1,44 @@
 package com.task.vendingmachine;
 
+import java.util.List;
+import java.util.Map;
+
+import com.task.vendingmachine.model.Beverage;
+import com.task.vendingmachine.model.Coin;
+
 public class VendingMachine {
+	
+	
+	public void addFund(Coin coin) {
+		
+	}
+	
+	public void addFund(List<Coin> coins) {
+		
+	}
+	
+	public void selectBeverage(String name) {
+		
+	}
+	
+	public void confirmPurchase() {
+		
+	}
+	
+	public List<Coin> getChange() {
+		return null;
+	}
+	
+	public List<Coin> cancelPurchase() {
+		return null;
+	}
+	
+	public void reset() {
+		
+	}
+	
+	public Map<Beverage, Integer> getConsumption() {
+		return null;
+	}
 	
 }
